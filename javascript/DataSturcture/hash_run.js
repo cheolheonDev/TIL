c9ee6ab1870eb312e3fef5987caa406624b8510b
cheolheonShin <cheolheonshin@gmail.com>
2020-02-03 13:46:11 +0900

    study: 충돌처리 - seperate chaining 기법

--- javascript/DataSturcture/hash_run.js
+++ javascript/DataSturcture/hash_run.js
@@ -1,6 +1,8 @@
 const HashTable = require('./simple_hash.js'); 
 const BetterHashTable = require('./better_hash.js');
+const SeperateChainingHashTable = require('./seperateChainingHash.js');
 var someNames = "David Jennifer Donnie Raymond Cynthia Mike Clayton Danny Jonathan".split(" ");
+var phoneNums = "023452 1234125 1235125 1235213 123425 6623456 1234523 2346236 1252345".split(" ");
 
 console.log('simple hash table'); 
 var hTable = new HashTable(); 
@@ -15,3 +17,10 @@ for(var i = 0; i< someNames.length; i++){
     bhTable.put(someNames[i]); 
 }
 bhTable.showDistro();
+
+console.log('seperate chaining hash table');
+var schTable = new SeperateChainingHashTable(); 
+for(var i = 0; i< someNames.length; i++){
+    schTable.put(someNames[i], phoneNums[i]); 
+}
+schTable.showDistro();
