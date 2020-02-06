826ddfb86a898394730dff241b7c5415119e0157
cheolheonShin <cheolheonshin@gmail.com>
2020-02-06 14:39:11 +0900

    study: 검색 - 자체 정렬 검색1, (파레토 비율적용 2)

--- javascript/DataSturcture/search/runSeqsearch.js
+++ javascript/DataSturcture/search/runSeqsearch.js
@@ -1,6 +1,8 @@
 const seqSearch = require('./seqsearch');
 const findMin = require('./findmin.js'); 
 const findMax = require('./findmax.js'); 
+const selfOrganizedSeqsearch = require('./selfOrganizedSeqsearch.js'); 
+const selfOrganizedSeqsearch2 = require('./selfOrganizedSeqsearch2.js'); 1
 
 function dispArr(arr){
     var log = '';
@@ -32,3 +34,16 @@ console.log('배열 내에서 최소값은 ');
 console.log(findMin(nums));
 console.log('배열 내에서 최대값은 '); 
 console.log(findMax(nums));
+
+var short_nums = [ 5, 1, 100, 4, 3, 10, 9, 7, 11, 14]; 
+console.log(' self organized, seq search : find number 100 ');
+for (var i = 1 ; i <= 2 ; i++){
+    selfOrganizedSeqsearch(short_nums,100);
+    console.log(short_nums);
+}
+
+console.log('self organized, seq search 2 : find number 1 (20% 앞에 해당하는 부분, 위치변경이 없다)');
+for (var i = 1 ; i <= 2 ; i++){
+    selfOrganizedSeqsearch2(short_nums,1);
+    console.log(short_nums);
+}
