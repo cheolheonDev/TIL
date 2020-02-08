d715c850664d720d824c1c13009743f21e71a11b
cheolheonShin <cheolheonshin@gmail.com>
2020-02-08 21:12:33 +0900

    study: 선택 정렬

--- javascript/DataSturcture/sorting/runSort.js
+++ javascript/DataSturcture/sorting/runSort.js
@@ -1,6 +1,8 @@
 const CArray = require('./CArray.js'); 
 const bubbleSort = require('./bubbleSort.js');
 const insertionSort = require('./insertionSort.js');
+const selectionSort = require('./selectionSort.js');
+
 
 
 console.log('=== bubble sort');
@@ -23,5 +25,16 @@ console.log('= before sorted');
 console.log(mynums.toString());
 console.log('sorting...');
 insertionSort(mynums);
+console.log('= sorted');
+console.log(mynums.toString());
+
+console.log('=== selection sort');
+var numElements = 10; 
+var mynums = new CArray(numElements);
+mynums.setData(); 
+console.log('= before sorted');
+console.log(mynums.toString());
+console.log('sorting...');
+selectionSort(mynums);
 console.log('= sorted');
 console.log(mynums.toString());
---
+++ javascript/DataSturcture/sorting/selectionSort.js
@@ -0,0 +1,16 @@
+function selectionSort(carray){
+    var min ; 
+    for(var outer = 0; outer <= carray.dataStore.length-2; ++outer){
+        min = outer; 
+        for(var inner = outer + 1; 
+            inner <= carray.dataStore.length-1; ++inner){
+                if(carray.dataStore[inner] < carray.dataStore[min])
+                {
+                    min = inner;
+                }
+            }
+        carray.swap(carray.dataStore, outer, min);
+        console.log(carray.toString());
+    }
+}
+module.exports = selectionSort;
