b4c3d577bd55295a3fac8e1747187de09989fd31
cheolheonShin <cheolheonshin@gmail.com>
2020-02-11 13:58:58 +0900

    study: knapsack 탐욕알고리즘으로 풀기 ( 물건 연속적임 )

--- javascript/DataSturcture/advanced/knapsack.js
+++ javascript/DataSturcture/advanced/knapsack.js
@@ -47,3 +47,27 @@ console.log('최대로 담는 가방');
 console.log(knapsack(capacity, size, value, n));
 console.log('최대로 담는 가방 (dynamic)');
 console.log(dknapsack(capacity, size, value, n));
+
+function gknapsack(values, weights, capacity ){
+    var load = 0; 
+    var i = 0; 
+    var w = 0; 
+    while( load < capacity && i < 4){
+        if(weights[i] <= (capacity -load)){
+            w += values[i];
+            load += weights[i]; 
+        }else{
+            var r = (capacity -load)/weights[i]; 
+            w += r*values[i]; 
+            load += weights[i];
+        }
+    ++i;
+    }
+    return w;
+}
+
+var items = ['a', 'b', 'c','d'];
+var values = [50, 140, 60, 60]; 
+var weights = [5,20, 10, 12];
+var capacity = 30;
+console.log(gknapsack(values, weights, capacity));
