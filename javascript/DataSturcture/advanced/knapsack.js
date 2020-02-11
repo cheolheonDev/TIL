18ecc6de1df15407ebd55d735e98bccfb202388f
cheolheonShin <cheolheonshin@gmail.com>
2020-02-11 13:49:47 +0900

    study: knapsack 문제 동적프로그래밍으로 풀기

--- javascript/DataSturcture/advanced/knapsack.js
+++ javascript/DataSturcture/advanced/knapsack.js
@@ -16,10 +16,34 @@ function knapsack(capacity, size, value, n){
     }
 }
 
-var value = [4,5,10,11,13];
-var size = [3,4,7,8,9];
+function dknapsack(capacity, size, value, n){
+    var K = []; 
+    for( var i = 0; i <= capacity+1 ; i++){
+        K[i] = [];
+    }
+    for(var i = 0; i <= n ; i++){
+        var str = '';
+        for(var w = 0; w <= capacity ; w++ ){
+            if(i == 0 || w == 0){
+                K[i][w] = 0;
+            }
+            else if( size[i-1] <= w ){
+                K[i][w] = max(value[i-1] + K[i-1][w-size[i-1]], K[i-1][w]);
+            }else{
+                K[i][w] = K[i-1][w];
+            }
+            str += K[i][w] + ' ';
+        }
+        console.log(str);
+    }
+    return K[n][capacity];
+}
+
+var value = [4,5,10,11,13,1000];
+var size = [3,4,7,8,9,15];
 var capacity = 16; 
-var n = 5;
+var n = 6;
 console.log('최대로 담는 가방');
 console.log(knapsack(capacity, size, value, n));
-
+console.log('최대로 담는 가방 (dynamic)');
+console.log(dknapsack(capacity, size, value, n));
