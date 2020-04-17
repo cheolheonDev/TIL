e4a060969f99e2c78939470482eb300b10e69b39
cheolheonShin <cheolheonshin@gmail.com>
2020-04-17 22:56:21 +0900

    study: ch5: max_min

---
+++ java/ch5/prac1/ArrayEx6.java
@@ -0,0 +1,18 @@
+class ArrayEx6 {
+    public static void main(String[] args) {
+        int[] score = { 79, 88, 91, 33, 100, 55, 95 };
+        int max = score[0];
+        int min = score[1];
+
+        for (int i = 1; i < score.length; i++) {
+            if (max < score[i]) {
+                max = score[i];
+            }
+            if (min > score[i]) {
+                min = score[i];
+            }
+        }
+        System.out.println("최대값 : " + max);
+        System.out.println("최소값 : " + min);
+    }
+}
